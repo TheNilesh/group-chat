89627899e8e55b127996c21922f027e923a3a5f2
Nilesh Akhade <akhadenilesh93@gmail.com>
2015-01-08 21:04:18 +0530

    Multi-threaded server

--- client.java
+++ client.java
@@ -6,16 +6,39 @@ class client
 client()
 {
 	//send msg to server
-	try{
-	Socket s=new Socket("127.0.0.1",1005);
-	DataInputStream dis=new DataInputStream(s.getInputStream());
-	DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-	dos.writeUTF("Who are You ?");
-	System.out.println("Server says: " + dis.readUTF());
+	try
+	{
+		Socket s=new Socket("127.0.0.1",1005);
+		System.out.println("Connection established");
+		DataInputStream dis=new DataInputStream(s.getInputStream());
+		DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+		String temp;
+		System.out.println("Chat session created..(bye to exit)");
+		do
+		{	
+			System.out.print("you: ");	
+			temp=getString();
+			dos.writeUTF(temp);
+			System.out.println("Server: " + dis.readUTF());
+		}while(!temp.equalsIgnoreCase("bye"));
+		s.close();
 	}catch(IOException ex) { System.out.println(ex);}
 }
+
 public static void main(String args[])
 {
 	new client();
 }
+
+static String getString() throws IOException
+ {
+   System.in.skip(System.in.available());
+
+   byte arr[] = new byte[512];
+   int x;
+   x= System.in.read(arr);
+   String temp = new String(arr, 0,x-1);
+   return temp;
+ }
+
 }
--- server.java
+++ server.java
@@ -4,23 +4,61 @@ import java.io.*;
 class server
 {
 server()
- {
-  //create Server
+{
+	Socket s;
+	int count,clcnt;
 	try
 	{
-	ServerSocket srv=new ServerSocket(1005);
-	Socket s=srv.accept();
-	DataInputStream dis=new DataInputStream(s.getInputStream());
-	DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-	String temp=dis.readUTF();	
-	dos.writeUTF("OK");
-	System.out.println("Client: " + temp);
+	  	ServerSocket srv=new ServerSocket(1005);
+		count=0;
+		while(count<10)
+		{
+			System.out.println("ONLINE");
+			s=srv.accept();
+			clcnt =Thread.activeCount();
+			System.out.println("Currently Serving " + clcnt + " clients.");
+			System.out.println("Total request accepted = " + ++count );
+			System.out.println("Got a request from ");
+			new processor(s);	//It will serve him
+		}
+		System.out.println("I am away for routine check-up!");
+		srv.close();
 	}
-	catch(IOException ex)
-	{System.out.println(ex);}
- }
+	catch(IOException ex) { System.out.println(ex);}
+}
+
 public static void main(String args[])
 	{
 	  new server();
 	}
+}//server
+
+class processor extends Thread
+{
+	Socket s;
+	processor(Socket t)
+	{
+		s=t; //this socket corresponds to client, serve it
+		start();
+		System.out.println("Just now I allocated one thread to serve this request. Bye");
+	}
+public void run()
+{
+	try
+	{
+		DataInputStream dis=new DataInputStream(s.getInputStream());
+		DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+		String temp;
+		do
+		{
+			temp=dis.readUTF();
+//			System.out.println("Client: " + temp);
+			dos.writeUTF(temp.toUpperCase());
+		}while(!temp.equalsIgnoreCase("bye"));
+		System.out.println("Somebody left.");
+		s.close();
+	}
+	catch(IOException ex)
+	{System.out.println(ex);}
+}
 }
